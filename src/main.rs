4ab6a95d534ee9b0b7aae15b5349c7df89fb7060
Tyler Slabinski <tslabinski@slabity.net>
2016-12-10 01:05:06 -0500

    Added boilerplate code to start errors and logging

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,28 @@ extern crate time;
 
 mod logger;
 
-fn main() {
-    logger::init().expect("Could not initialize logger");
-    use glium::DisplayBuild;
-    let display = match glium::glutin::WindowBuilder::new().build_glium() {
-        Ok(d) => d,
-        Err(e) => panic!(e)
-    };
+error_chain!{}
+
+fn run() -> Result<()> {
+    Ok(())
 }
+
+fn main() {
+    // Before we do anything. Initialize the logger.
+    // This will only fail if we can't write to stderr.
+    logger::init().expect("Could not initialize logger");
+
+    // Run the program, and enter the block if we get an error.
+    if let Err(ref e) = run() {
+        error!("Program failed: {}", e);
+
+        // Backtrace if we can. We may need RUST_BACKTRACE=1
+        if let Some(backtrace) = e.backtrace() {
+            debug!("{:?}", backtrace);
+        }
+
+        // Exit with error code 1
+        ::std::process::exit(1);
+    }
+}
+
